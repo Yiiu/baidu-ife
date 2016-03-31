39401afd5b2b0d6ac956ca9572e97da928b73e7e
Yu <yu.gintoki@gmail.com>
2016-03-31 21:59:25 +0800

    task--2-21

--- task-2-21/task.js
+++ task-2-21/task.js
@@ -1,24 +1,24 @@
 var arr = [];               //标签
 var conarr = [];         //兴趣爱好
-var bb = []; //临时数据
 /*
  * 吧字符串分割开来
  * 回车，逗号（全角半角均可），顿号，空格（全角半角、Tab等均可）等符号
  */ 
+/*  */
 /* 吧字符分隔开，批量插入 */
 function te(obj){
     cc = obj.value.trim();
-    bb = cc.split(/[\s|,|，|、|　]+/);
+    arr = cc.split(/[\s|,|，|、|　]+/);
     var num = 0;
-    for(var i=0;i<bb.length;i++){
-        for(var j=i+1;j<=bb.length;j++){
-            if(bb[i] === bb[j]){
-                bb.splice(j,1);
-                alert(bb[j]);
+    var count =0;
+    for(var i=0;i<=arr.length;i++){
+        for(var j=i;j<arr.length;j++){
+            if(arr[j] === arr[i]){
+                count++;
+                arr.push(arr[j]);
             }
         }
     }
-    arr.push(bb);
     console.log(arr);
 }
 /* 输出 */
