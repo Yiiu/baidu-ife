431860cac47db3ad1b6c6163a4379974d6ad4ec6
Yu <yu.gintoki@gmail.com>
2016-04-02 20:03:08 +0800

    task-2-21
    兴趣 完成

--- task-2-21/task.js
+++ task-2-21/task.js
@@ -1,5 +1,6 @@
 var arr = [];               //标签
 var conarr = [];         //兴趣爱好
+var tagcnt = document.getElementsByClassName("tags")[0];
 /*
  * 吧字符串分割开来
  * 回车，逗号（全角半角均可），顿号，空格（全角半角、Tab等均可）等符号
@@ -7,36 +8,34 @@ var conarr = [];         //兴趣爱好
 /*  */
 /* 吧字符分隔开，批量插入 */
 function te(obj){
-    cc = obj.value.trim();
-    arr = cc.split(/[\s|,|，|、|　]+/);
-    var num = 0;
-    var count =0;
-    for(var i=0;i<=arr.length;i++){
-        for(var j=i;j<arr.length;j++){
-            if(arr[j] === arr[i]){
-                count++;
-                arr.push(arr[j]);
-            }
+    var cc = obj.value.trim();
+    var bb = cc.split(/[\s|,|，|、|　]+/);
+    for(var i=0;i<bb.length;i++){
+        if(!arr.eqArray(bb[i])){
+            arr.push(bb[i]);            
         }
     }
+    div(tagcnt);
     console.log(arr);
 }
 /* 输出 */
 function div(obj){
-    obj.innerHTML = ""
+    var tag = document.getElementById("tag-1");
+    tag.value = "";
+    obj.innerHTML = "";  
     for(var i=0;i<arr.length;i++){
         obj.innerHTML += "<span id='"+i+"'>"+ arr[i] +"</div>";
     }
+    del(obj);
 }
 // 按钮的动作
 function open(){
     /* 标签的回车事件 */
     var tag = document.getElementById("tag-1");
-    var tagcnt = document.getElementsByClassName("tags")[0];
     tag.addEventListener("keydown",function(e){
         if(event.keyCode==13){
+            var sp = document.getElementById
             nulls(tag);
-            div(tagcnt);
         }
     })
 }
@@ -61,4 +60,20 @@ function del(obj){
 //去除空格
 String.prototype.trim=function() {
     return this.replace(/(^\s*)|(\s*$)/g,'');
+}
+Array.prototype.eqArray = function(e){
+    var num = 0;
+    for(var i=0;i<this.length;i++){
+        if(this[i] === e){
+            num++;
+        }
+    }
+    if(num == 0){
+        return false;
+    }else{
+        return true;
+    }
+}
+window.onload = function(){
+    open()
 }
