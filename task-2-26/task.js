1f0986e6039daf4860aa90959eeaa136c11cbb57
Yu <yu.gintoki@gmail.com>
2016-04-17 20:21:58 +0800

    task-2-26 vessel
    task-2-26 vessel

--- task-2-26/task.js
+++ task-2-26/task.js
@@ -1,49 +1,96 @@
-
 var vessel = function(obj){
   this.element = obj.element;
   this.prepare = obj.prepare;   // 飞船状态 true false true:在 false: 不在
   this.status = obj.status;     // 飞行的状态 true false
   this.energy = 100;            // 能源，初始100能源，飞行时间减少能源
+  this.jo = 0;                  // 到达的地点，初始起点0
 }
 vessel.prototype = {
   //飞船状态
   // false 不在 true 在
   pre:function(){
-    var sty = this.element.children[0];
     if(this.prepare === true){
       this.prepare = false;
-      sty.className += " none";
+      this.element.className += " none";
     }else{
-      sty.className = sty.className.replace(/ none/,"")
+      this.element.className = this.element.className.replace(/ none/,"");
       this.prepare = true;
-      console.log(sty);
     }
     return this
   },
   //飞船飞行态
   // false 不在飞行 true 在飞行
   sta:function(){
-    if(this.status === true){
-      this.fl();
+    if(this.status == true){
       this.status = false;
+      this.stop();
     }else{
-      this.fl();
       this.status = true;
+      this.fl();
     }
-    return this
   },
   // 飞行
   fl:function(){
-    if(this.prepare === true && this.prepare === true){
-      var timer = setInterval(function(){
-        if(this.energy <=0 ){
-          clearInterval(timer);
-        }else{
-          this.style.transform = "roate("+10+"deg)"
+    if(this.prepare == true && this.status == true){
+      var e = this;
+      var en = childs(this.element,"energy");
+      var baifen = childs(en,"baifen");
+      var speed = 1;      //速度
+      clearInterval(e.Stimer);
+      if(this.Ftimer){
+        clearInterval(this.Ftimer);
+      }
+      this.Ftimer = setInterval(function(){
+        if(e.energy > 0){
+          e.energy = e.energy-1;
+          baifen.style.width = e.energy+"%";
+          e.jo+=speed;
+          e.element.style.webkitTransform = "rotate("+ e.jo +"deg)"
         }
-      },1000);
+        if(e.energy == 0){
+          e.supply();
+        }
+      },50);
     }
-  }
-  ,
+  },
+  //补给,燃料为0自动填充
+  supply:function(){
+    if(this.energy == 0){
+      var flag = false;
+      var en = childs(this.element,"energy");
+      var baifen = childs(en,"baifen");
+      var e = this;
+      clearInterval(this.Ftimer);
+      if(this.Stimer){
+        clearInterval(this.Stimer);
+      }
+      this.Stimer = setInterval(function(){
+        e.energy = e.energy+1;
+        baifen.style.width = e.energy+"%";
+        if(e.energy >= 100){
+          e.energy = 100;
+          e.fl();
+        }
+      },100);
+    }
+  },
+  // 停止飞行
+  stop:function(){
+    if(this.Ftimer){
+      clearInterval(this.Ftimer);
+    }
+    if(this.Stimer){
+      clearInterval(this.Stimer);
+    }
+  },
 }
 var a = new vessel({element:document.getElementsByClassName("vessels")[0],prepare:false,status:false});
+function childs(obj,cls){
+  for(var i = 0;i<obj.children.length;i++){
+    if(obj.children[i].className == cls){
+      return obj.children[i];
+    }else if(obj.children[i].children.length != 0){
+      childs(obj.children[i],cls);
+    }
+  }
+}
